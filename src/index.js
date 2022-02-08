0d1e701d0d38e063f8db5fed5f273e269f57fdce
ekaterinalutik <ekaterinalutik@gmail.com>
2022-02-08 17:00:45 +0200

    feat: problem solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let str_brackets = bracketsConfig.join('').replace(/,/g,'');
+  let empty_stack = [];
+  for (let str_bracket of str) {
+    let str_bracketsIndex = str_brackets.indexOf(str_bracket)
+    if (str_bracketsIndex % 2 === 0) {
+      if (str_bracket === str_brackets[str_bracketsIndex + 1] && empty_stack[empty_stack.length - 1] === str_bracketsIndex){
+        empty_stack.pop();
+      }
+      else {
+        empty_stack.push(str_bracketsIndex)
+      }
+    }
+    else {
+      if(empty_stack.pop() !== str_bracketsIndex -1) {
+        return false;
+      }
+    }
+  } return empty_stack.length === 0;
 }
